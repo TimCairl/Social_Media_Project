0073acc9039ed54537e06fd06c77f14296fcc5ef
unknown <aleks.stepien2@gmail.com>
2018-02-14 01:31:20 -0500

    I have no idea what I'm doing. I started doing something but I got stuck.

--- View/page_createAccount.php
+++ View/page_createAccount.php
@@ -1,8 +1,80 @@
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+
+</style>
+</head>
+
 <body>
 
-<form action="../DAL/DAL_confirmPassword.php">
+<?php
+
+//userId	username	password	firstName	lastName	dateOfBirth	bio	interest	job	employeer	isSuspended	isPublic	profilePicture
+
+
+$username = $password = $con_password = $firstname = $lastname = $bday = $privacy = "";
+$userERR = $passERR = $fnameERR = $lnameERR = $bdayERR = "";
+
+function test_data($data) 
+{
+  /* 
+  I might have to change/remove this. It's from here:
+  https://www.w3schools.com/php/showphp.asp?filename=demo_form_validation_complete
+  */
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+function create_account($uname, $pass, $fname, $lname, $dob, $bio, $interest, $job, $employer, $suspended, $public, $pic)
+{
+  
+}
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+  //Username
+  if(empty($_POST["username"]))
+  {
+    $userERR = "Enter a Username";
+  }
+  else
+  {
+    $username = test_data($_POST["username"]);
+  }
+
+  //Password
+  if(empty($_POST["password"]))
+  {
+    $passERR = "Enter a Password";
+  }
+  else
+  {
+    $password = test_data($_POST["password"]);
+    if(!preg_match("/^[a-zA-Z0-9]*$/", $password))
+    {
+      $passERR = "Passwords can only contain letters and numbers (maybe change this later)";
+    }
+    else
+    {
+      $con_password = test_data($_POST["con_password"]);
+    }
+  }
+
+}
+
+
+
+
+
+
+?>
+
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
   <fieldset>
     <legend>User Information:</legend>
     
@@ -44,6 +116,5 @@
 
   </fieldset>
 </form>
-
 </body>
 </html>
