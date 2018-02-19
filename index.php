3bb77163a370af909481b9dd83e3d3edbaa79174
Tim Cairl <timcairl911@gmail.com>
2018-02-19 18:42:03 -0500

    more edits to fix the title

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $AppSettingsRepository = new AppSettingsRepository();
 
 $AppName = $AppSettingsRepository->pullAppNameFromDatabase();
 echo $AppName;
-echo "<title>".$AppName."</title>";
+echo "<head><title>".$AppName."</title></head>";
 echo "<body>
 
 <h1>This is a heading</h1>
