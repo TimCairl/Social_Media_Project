d1b7aea8a19efa010db3e038259f7d0a59925749
Tim Cairl <timcairl911@gmail.com>
2018-03-10 16:12:36 -0500

    Removed daltest file since we have working dals. also renamed column in table from "employeer" to "employer"

--- DAL/DALtest.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "social_media";
-    $connection = new mysqli($server, $username, $password, $dbname);
-    $sql = "insert into users (username, password, firstName, lastName) values('test123', 'password123', 'Joe', 'Schmoe')";
-    $connection->query($sql);
-?>
--- Repository/UserRepository.php
+++ Repository/UserRepository.php
@@ -65,7 +65,7 @@ class UserRepository extends Repository
             $UserModel->userBio = $row['bio'];
             $UserModel->userInterest = $row['interest'];
             $UserModel->userJob = $row['job'];
-            $UserModel->userEmployer = $row['employeer']; // 'employeer' in the table, should be changed to 'employer'
+            $UserModel->userEmployer = $row['employer'];
             $UserModel->userIsSuspended = $row['isSuspended'];
             $UserModel->userIsPublic = $row['isPublic'];
             $UserModel->userProfilePicture = $row['profilePicture'];
