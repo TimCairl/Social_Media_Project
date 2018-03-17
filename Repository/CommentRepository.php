f9ae7efcd734d629b51af0d373382a363b4e301f
unknown <aleks.stepien2@gmail.com>
2018-03-17 18:01:23 -0400

    More work on the post and comment repositories.

--- Repository/CommentRepository.php
+++ Repository/CommentRepository.php
@@ -1,8 +1,41 @@
 <?php
 require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/Social_Media_Project/Repository/Repository.php");
-require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/Social_Media_Project/Model/PostModel.php");
+require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/Social_Media_Project/Model/CommentModel.php");
 class CommentRepository extends Repository
 {
+    function pushCommentToDatabase($postID, $userID, $body, $timestamp)
+    {
+        $sql = "INSERT INTO comments (commentID, postID, userID, body, timestamp) 
+        VALUES ('$postID', '$userID', '$body', '$timestamp')";
+    
+        $this->connection->query($sql);
+    }
 
+    function pullCommentFromDatabase($commentID)
+    {
+        //Returns a single comment
+
+        $CommentModel = new CommentModel();
+
+        $sqlcommand = "SELECT * FROM comments WHERE commentID='$commentID'";
+        $result = $this->connection->query($sqlcommand);
+
+        if($result->num_rows > 0)
+        {
+            $row = $result->fetch_assoc();
+            $CommentModel->commentID = $row['commentID'];
+            $CommentModel->commentPostID = $row['postID'];
+            $CommentModel->commentUserID = $row['userID'];
+            $CommentModel->commentBody = $row['body'];
+            $CommentModel->commentTimestamp = $row['timestamp'];
+        }
+
+        return $PostModel;
+    }
+
+    function getCommentsWithPostID($postID)
+    {
+        //Returns an array of comments.
+    }
 }
 ?>
--- Repository/PostRepository.php
+++ Repository/PostRepository.php
@@ -3,6 +3,39 @@ require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/Social_Media_Project/Reposito
 require_once(realpath($_SERVER["DOCUMENT_ROOT"])."/Social_Media_Project/Model/PostModel.php");
 class PostRepository extends Repository
 {
+    function pushPostToDatabase($userID, $subject, $body, $timestamp)
+    {
+        $sql = "INSERT INTO posts (postID, userID, subject, body, timestamp) 
+        VALUES ('$userID', '$subject', '$body', '$timestamp')";
+    
+        $this->connection->query($sql);
+    }
+
+    function pullPostFromDatabase($postID)
+    {
+        $PostModel = new PostModel();
+
+        $sqlcommand = "SELECT * FROM posts WHERE postID='$postID'";
+        $result = $this->connection->query($sqlcommand);
+
+        if($result->num_rows > 0)
+        {
+            $row = $result->fetch_assoc();
+            $PostModel->postID = $row['postID'];
+            $PostModel->postUserID = $row['userID'];
+            $PostModel->postSubject = $row['subject'];
+            $PostModel->postBody = $row['body'];
+            $PostModel->postTimestamp = $row['timestamp'];
+        }
+
+        return $PostModel;
+    }
+
+    function getPostsWithUserID($userID, $timestamp)
+    {
+        //Returns an array of posts after the specified timestamp.   
+    }
+
 
 }
 ?>
